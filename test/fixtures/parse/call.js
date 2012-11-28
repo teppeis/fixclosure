3411ed1f5f6d7e20184fbf2b96337db3b30dbeaf
teppeis <teppeis@gmail.com>
2012-11-29 03:47:02 +0900

    Supports for a method starting with "$"

--- test/fixtures/parse/call.js
+++ test/fixtures/parse/call.js
@@ -1,8 +1,15 @@
+// start with a-z
 goog.foo.foo1();
 goog.foo.foo2();
 goog.bar.bar1();
 goog.bar.bar2();
+// start with $
+goog.baz.$();
+goog.baz.$$();
+goog.baz.$F();
+// only goog namespace should be require by default.
 foo.bar.bar2();
 
 // toRequire: goog.bar
+// toRequire: goog.baz
 // toRequire: goog.foo
