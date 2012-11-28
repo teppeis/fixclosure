074cf6d8185cd24f0162ffd929e6ccecb0d2a764
teppeis <teppeis@gmail.com>
2012-11-29 02:26:51 +0900

    Fix #6 False report that provided packages are missing

--- test/fixtures/parse/constructor.js
+++ test/fixtures/parse/constructor.js
@@ -24,14 +24,6 @@ goog.inherits(goog.ui.Bubble, goog.ui.Component);
 goog.ui.Bubble.prototype.hello = function() {
 };
 
-/**
- * Private class should not be provide.
- * @constructor
- * @private
- */
-goog.ui.Private_ = function() {
-};
-
 // toProvide: goog.ui.Bubble
 // toRequire: goog.ui.Component
 // toRequire: goog.ui.Popup
--- test/fixtures/parse/private_static_property.js
+++ test/fixtures/parse/private_static_property.js
@@ -1,13 +1,53 @@
 /**
+ * Private class should not be provide.
+ * @constructor
+ * @private
+ */
+goog.p1.Private_ = function() {
+};
+
+/**
+ * Nested private class.
+ * @constructor
+ * @private
+ */
+goog.p2.Private1_.Private2_.Private3_ = function() {
+};
+
+/**
+ * Method of nested private class.
+ */
+goog.p2.Private1_.Private2_.Private3_.prototype.hello = function() {
+};
+
+/**
+ * Static method of nested private class.
+ */
+goog.p2.Private1_.Private2_.Private3_.publicStatic = function() {
+};
+
+/**
+ * Private static property should not be require or provide.
+ * @private
+ */
+goog.p3.privateProp_ = goog.debug.Logger.getLogger('aaa');
+
+/**
  * Private static method should not be require or provide.
  * @private
  */
-goog.dom.logger_ = goog.debug.Logger.getLogger('aaa');
+goog.p3.privateMethod_ = function() {
+};
 
 /**
  */
-goog.dom.hello = function() {
-  goog.dom.logger_.info('bbb');
+goog.p4.hello = function() {
+  goog.p3.privateProp_.info('bbb');
+  goog.p3.privateMethod_();
 };
+
 // toRequire: goog.debug.Logger
-// toProvide: goog.dom
+// toProvide: goog.p1
+// toProvide: goog.p2
+// toProvide: goog.p3
+// toProvide: goog.p4
