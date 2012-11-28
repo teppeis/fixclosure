3bf2cc4630b8169cb27cb614d9210f61e9700d1d
teppeis <teppeis@gmail.com>
2012-11-29 04:29:34 +0900

    Support const property correctly

---
+++ test/fixtures/parse/constant.js
@@ -0,0 +1,22 @@
+/**
+ * @type {number}
+ * @const
+ */
+goog.cnst.NUM = 1;
+
+/**
+ * @type {number}
+ * @const
+ */
+goog.cnst.NUM_BAR = 2;
+
+/**
+ * @type {RegExp}
+ * @const
+ */
+goog.cnst.REGEX = /test/;
+
+// Should not be provide REGEX.
+goog.cnst.REGEX.test('test');
+
+// toProvide: goog.cnst
