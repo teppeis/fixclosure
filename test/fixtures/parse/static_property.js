97ea2c18f58f5e5adb2a3225a96e1dfd3c6d8225
teppeis <teppeis@gmail.com>
2012-12-01 22:26:15 +0900

    Support static property

---
+++ test/fixtures/parse/static_property.js
@@ -0,0 +1,9 @@
+/**
+ * @type {number}
+ */
+goog.debug.Console1.instance = 1;
+
+goog.debug.Console2.instance.setCapturing(true);
+
+// toProvide: goog.debug.Console1
+// toRequire: goog.debug.Console2
