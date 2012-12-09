2ed3e1f3b8b75a1261450b51877f8b2eb6e3a993
teppeis <teppeis@gmail.com>
2012-12-09 16:08:44 +0900

    Fix #8 support for @implements

---
+++ test/fixtures/parse/@implements.js
@@ -0,0 +1,18 @@
+/**
+ * @constructor
+ * @implements {goog.Disposable}
+ */
+var Foo = function() {
+};
+
+/**
+ * @implements {goog.fx.anim.Animated}
+ * @implements {goog.fx.Transition}
+ * @constructor
+ */
+var Foo = function() {
+};
+
+// toRequire: goog.Disposable
+// toRequire: goog.fx.Transition
+// toRequire: goog.fx.anim.Animated
