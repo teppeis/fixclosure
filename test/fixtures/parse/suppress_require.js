d35ac53106d5978b1f310530b4a2b54aabae505a
teppeis <teppeis@gmail.com>
2014-05-30 16:25:51 +0900

    Allow inline hints with comment

--- test/fixtures/parse/suppress_require.js
+++ test/fixtures/parse/suppress_require.js
@@ -2,6 +2,8 @@ goog.foo.foo();
 // fixclosure: suppressRequire
 goog.bar.bar();
 goog.baz.baz();
+// fixclosure: suppressRequire with comment
+goog.bao.bao();
 
 // toRequire: goog.baz
 // toRequire: goog.foo
