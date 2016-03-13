d75473f613679e9fcb20285d8f73a484f686f4d0
teppeis <teppeis@gmail.com>
2016-03-13 16:42:59 +0900

    test(jsx): improve testing for JSX

--- test/fixtures/parse/jsx.jsx
+++ test/fixtures/parse/jsx.jsx
@@ -1,6 +1,10 @@
-<goog.foo.Bar>
-  <goog.foo.Baz />
-</goog.foo.Bar>
+<goog.foo.Element attr={goog.foo.Attribute}>
+  <goog.foo.SelfClosing {...goog.foo.Spread}/>
+  {goog.foo.Content}
+</goog.foo.Element>
 
-// toRequire: goog.foo.Bar
-// toRequire: goog.foo.Baz
+// toRequire: goog.foo.Attribute
+// toRequire: goog.foo.Content
+// toRequire: goog.foo.Element
+// toRequire: goog.foo.SelfClosing
+// toRequire: goog.foo.Spread
