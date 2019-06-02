6e4824773a5220dc758dcac6b77f6190e7a2e32f
teppeis <teppeis@gmail.com>
2019-06-02 23:11:43 +0900

    feat: report goog.requireType() errors

--- test/fixtures/cli/all-ng-types.js
+++ test/fixtures/cli/all-ng-types.js
@@ -8,10 +8,21 @@ goog.require('goog.require.dup');
 goog.require('goog.require.ignore'); // fixclosure: ignore
 goog.require('goog.require.unnecessary');
 
-goog.provide.dup.foo = function() {
+goog.requireType('goog.requireType.dup');
+goog.requireType('goog.requireType.dup');
+goog.requireType('goog.requireType.ignore'); // fixclosure: ignore
+goog.requireType('goog.requireType.unnecessary');
+
+/**
+ * @param {goog.requireType.dup} a
+ */
+goog.provide.dup.foo = function(a) {
     goog.require.dup.foo();
 };
 
-goog.provide.missing.foo = function() {
+/**
+ * @param {goog.requireType.missing} a
+ */
+goog.provide.missing.foo = function(a) {
     goog.require.missing.foo();
 };
