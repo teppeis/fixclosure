20ba4628b7da0448344f284f5dafe27d98e70693
teppeis <teppeis@gmail.com>
2019-07-07 16:16:04 +0900

    feat: export getFixedSource()

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 const Parser = require("./lib/parser");
 const cli = require("./lib/cli");
-const fix = require("./lib/fix");
+const { fixInPlace, getFixedSource } = require("./lib/fix");
 
-module.exports.Parser = Parser;
-module.exports.fix = fix;
-module.exports.cli = cli;
+module.exports = {
+  Parser,
+  cli,
+  fixInPlace,
+  getFixedSource,
+  // for backward compatibility
+  fix: fixInPlace,
+};
 
 // TODO: suppress warning
 // TODO: namespace re-assignment
