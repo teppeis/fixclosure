41c6822687a24292992817f4167287b631e967d5
teppeis <teppeis@gmail.com>
2022-05-26 23:17:47 +0900

    fix!: cleanup default replaceMap

--- src/default.ts
+++ src/default.ts
@@ -3,15 +3,10 @@ const roots = ["goog", "proto2", "soy", "soydata", "svgpan"];
 const replaceMap = {
   "goog.Disposable.MonitoringMode": "goog.Disposable",
   "goog.Promise.CancellationError": "goog.Promise",
-  "goog.debug.GcDiagnostics_": "goog.debug.GcDiagnostics",
   "goog.debug.Trace_": "goog.debug.Trace",
-  "goog.disposeAll": "goog.dispose",
-  "goog.dom.$F": "goog.dom.forms.getValue",
   "goog.dom.BufferedViewportSizeMonitor.EventType":
     "goog.dom.BufferedViewportSizeMonitor",
   "goog.editor.Plugin.Op": "goog.editor.Plugin",
-  "goog.ui.KeyboardShortcutHandler.Modifiers":
-    "goog.ui.KeyboardShortcutHandler",
   "goog.ui.SplitPane.EventType": "goog.ui.SplitPane",
 };
 
