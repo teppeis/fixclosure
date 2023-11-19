3ebb0567e66d5557cb3604337725561f63df64ba
teppeis <teppeis@gmail.com>
2023-11-19 16:56:44 +0900

    test: add eslint mocha config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import { build } from "eslint-config-teppeis";
+import { mocha } from "eslint-config-teppeis/configs/mocha";
 
 export default await build(
   { base: "node18", typescript: true },
   {
     ignores: ["lib", "test/fixtures"],
   },
+  mocha,
 );
